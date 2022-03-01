549c66cf2500829e1b1c0c21cfa2a9f02d44eb13
Cipta Adinagara <ciptaadinagara@gmail.com>
2022-03-01 16:09:11 +0700

    Update

--- scrap-1.py
+++ scrap-1.py
@@ -1,6 +1,8 @@
-import os
-import requests
-from bs4 import BeautifulSoup
+import os #Membuat temporary file
+import requests #Untuk mengambil website
+import json #Untuk membuat list
+from bs4 import BeautifulSoup #Untuk mengubah data website
+import pandas as pd #Untuk Memasukan ke CSV dan Excel
 
 #Input Url, Parameter, dan Header
 url = 'https://www.indeed.com/jobs?'
@@ -15,11 +17,10 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 
 res = requests.get(url, params=params, headers=headers)
 
-
-def get_total_pages():
+def get_total_pages(query, location):
     params = {
-        'q': 'Python Developer',
-        'l': 'New York State',
+        'q': query,
+        'l': location,
         'vjk': '71acc67a281f6038'
     }
 
@@ -46,10 +47,11 @@ def get_total_pages():
     print(total)
     return total
 
-def get_all_items():
+def get_all_items(query, location, start, page):
     params = {
-        'q': 'Python Developer',
-        'l': 'New York State',
+        'q': query,
+        'l': location,
+        'start' : start,
         'vjk': '71acc67a281f6038'
     }
 
@@ -76,7 +78,7 @@ def get_all_items():
         try:
             company_link = site + company.find('a')['href']
         except:
-            company_link = 'Link is not avalable'
+            company_link = 'Link is not available'
 
         #sorting data
         data_dict = {
@@ -86,7 +88,62 @@ def get_all_items():
         }
         job_list.append(data_dict)
 
-    print('Jumlah Data: ', len(job_list))
+    #Membuat JSON
+    try:
+        os.mkdir('json_result')
+    except FileExistsError:
+        pass
+
+    with open(f'json_result/{query}_in_{location}_page_{page}.json', 'w+') as json_data:
+        json.dump(job_list, json_data, indent=2)
+    print('json created')
+    return job_list
+
+    #Membuat CSV / Excel
+    """df = pd.DataFrame(job_list) #Contoh1
+    df.to_csv('indeed_data.csv', index=False)
+    df.to_excel('indeed_data.xlsx', index=False)
+    print('Data Created Successfully')
+    """
+
+def create_document(dataFrame, filename):
+    try:
+        os.mkdir('data_result')
+    except FileExistsError:
+        pass
+
+    df = pd.DataFrame(dataFrame)
+    df.to_csv(f'data_Result/{filename}.csv', index=False)
+    df.to_excel(f'data_Result/{filename}.xlsx', index=False)
+
+    print(f'File {filename}.csb and {filename}.xlsx successfully created')
+
+def run():
+    query = input('Enter Your Query: ')
+    location = input('Enter Your Location: ')
+
+    total = get_total_pages(query, location)
+    counter = 0
+    final_result =[]
+
+    for page in range(total):
+        page += 1
+        counter +=10
+        final_result += get_all_items(query, location, counter, page)
+
+    #Format Data
+    try:
+        os.mkdir('reports')
+    except FileExistsError:
+        pass
+
+    with open('reports/{}.json'.format(query), 'w+') as final_data:
+        json.dump(final_result, final_data, indent = 2)
+
+    print('Data Json Created')
+
+    #Create Document
+    create_document(final_result, query)
 
 if __name__ == '__main__':
-    get_all_items()
+    run()
